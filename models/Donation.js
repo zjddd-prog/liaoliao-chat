4bbfdd99d1ab4569e5f0dcf3f9b2837070886328
zjddd-prog <3956652638@qq.com>
2026-06-30 18:36:46 +0800

    飞友之家 v3.0: 删除旧Mongoose模型（已迁移至PostgreSQL）

--- models/Donation.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const donationSchema = new mongoose.Schema({
-    _id: { type: String, default: 'donation_config' },
-    wechat: { type: String, default: '' },
-    alipay: { type: String, default: '' }
-});
-
-module.exports = mongoose.model('Donation', donationSchema);
